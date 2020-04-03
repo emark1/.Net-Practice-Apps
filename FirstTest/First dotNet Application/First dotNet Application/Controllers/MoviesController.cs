cf92cee42f4a62b447b7092b091e8d71ca9d2904
emark1 <devel010@umn.edu>
2020-04-03 18:58:06 -0500

    Routes

--- FirstTest/First dotNet Application/First dotNet Application/Controllers/MoviesController.cs
+++ FirstTest/First dotNet Application/First dotNet Application/Controllers/MoviesController.cs
@@ -21,5 +21,33 @@ namespace First_dotNet_Application.Controllers
 
             return View(movie);
         }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
     }
 }
